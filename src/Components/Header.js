43f3e4122b66dc87e22e1dc32d1021e3285ce569
Brollan <patrikbrolin@hotmail.com>
2022-06-04 23:48:02 +0200

    First commit

---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Headers.css";
+
+const Header = (props) => {
+	return (
+		<header>
+			<ul>
+				<li>Hem</li>
+				<li>Om mig</li>
+				<li>Projekt</li>
+				<li>Kontakt</li>
+			</ul>
+		</header>
+	);
+};
+
+export default Header;
