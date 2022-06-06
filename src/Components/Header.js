40b6bd02ca37327d30aaa70c0838fca6fae0e443
Brollan <patrikbrolin@hotmail.com>
2022-06-06 10:27:57 +0200

    funktionalitet och responsivitet för meny

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,53 @@
-import React from "react";
-import "./Headers.css";
+import React, {useState, useEffect} from "react";
+import classes from "./Headers.module.css";
 
 const Header = (props) => {
+	const [menuState, setMenuState]  = useState(false)
+
+	useEffect(() => {
+		function handleResize(){
+			if(window.innerWidth > 600){
+				setMenuState(false)
+			} else{
+				setMenuState(true)
+			}
+	
+		}
+		window.addEventListener('resize', handleResize)
+
+		return _ => {
+      window.removeEventListener('resize', handleResize)
+		}
+	
+	})
+
+	
+
+	const menuHandler = () => {
+		!menuState ? setMenuState(true): setMenuState(false)
+	}
+
 	return (
-		<header>
-			<ul>
-				<li>Hem</li>
-				<li>Om mig</li>
-				<li>Projekt</li>
-				<li>Kontakt</li>
+		<header className={classes.header}>
+			<ul className={!menuState ? classes.ul : classes.ul__active}>
+				<li className={classes.li}>
+				<a href="/">Hem</a>
+				</li>
+				<li className={classes.li}>
+				<a href="/">Om mig</a>
+				</li>
+				<li className={classes.li}>
+				<a href="/">Projekt</a>
+				</li>
+				<li className={classes.li}>
+					<a href="/">Kontakt</a>
+				</li>
 			</ul>
+			<div className={classes.header__content__mobile} onClick={menuHandler}>
+				<span className={menuState ? classes.header__content__mobile__span : classes.header__content__mobile__span__active} ></span>
+				<span className={menuState ? classes.header__content__mobile__span : classes.header__content__mobile__span__active}></span>
+				<span className={menuState ? classes.header__content__mobile__span : classes.header__content__mobile__span__active}></span>
+			</div>
 		</header>
 	);
 };
