3e0253e00999ef04266378c0fa2d3e83720afad3
Brollan <patrikbrolin@hotmail.com>
2022-06-21 15:13:59 +0200

    styling project component

--- src/Components/ProjectPage/Projects.js
+++ src/Components/ProjectPage/Projects.js
@@ -6,11 +6,18 @@ const Projects = (props) => {
     <div className={classes.Projects} ref={props.reference}>
       <div className={classes.projects__wrapper}>
         <a  className={classes.project__t_paita}target='_blank' href='https://github.com/PatrikBrolin/School-project-Webshop'>
+          <div className={classes.project__t_paita__img}></div>
         </a>
         
-        <a className={classes.project__banken} target='_blank' href='https://github.com/PatrikBrolin/Banken'></a>
-        <a className={classes.project__bookduck} target='_blank' href='https://github.com/PatrikBrolin/BookDuck'></a>
-        <a className={classes.project__paltbyrå} target='_blank' href='https://github.com/PatrikBrolin/School-project-HTML-and-CSS'></a>
+        <a className={classes.project__banken} target='_blank' href='https://github.com/PatrikBrolin/Banken'>
+        <div className={classes.project__banken__img}></div>
+        </a>
+        <a className={classes.project__bookduck} target='_blank' href='https://github.com/PatrikBrolin/BookDuck'>
+        <div className={classes.project__bookduck__img}></div>
+        </a>
+        <a className={classes.project__paltbyrå} target='_blank' href='https://github.com/PatrikBrolin/School-project-HTML-and-CSS'>
+        <div className={classes.project__paltbyrå__img}></div>
+        </a>
       </div>
     </div>
   );
